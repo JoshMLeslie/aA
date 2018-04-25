6350a13f6ae2267753856fc270435c7fd427628f
Andrew Schumacher <as228512@ohio.edu>
2018-04-25 18:00:51 -0400

    final W6D3 update

--- W6D3/glitter/app/assets/javascripts/bundle.js
+++ W6D3/glitter/app/assets/javascripts/bundle.js
@@ -167,9 +167,23 @@ module.exports = FollowToggle;
 class UsersSearch {
   constructor ($el) {
     this.$el = $el;
-    debugger
-    const $input = $('.users-search input').prop("value");
+    this.$input = $('.users-search input').prop("value");
+    this.$ul = $('.users-search ul');
     // this.input = $el.data();
+  }
+
+  searchUsers(queryVal, success) {
+    $.ajax({
+      url: "users/search",
+      dataType: "json",
+      method: "GET",
+      data: {queryVal}
+    });
+  }
+
+  handleInput () {
+    // input? do things?
+    this.searchUsers(/*more things*/);
   }
 
 }
--- W6D3/glitter/frontend/users_search.js
+++ W6D3/glitter/frontend/users_search.js
@@ -1,9 +1,23 @@
 class UsersSearch {
   constructor ($el) {
     this.$el = $el;
-    debugger
-    const $input = $('.users-search input').prop("value");
+    this.$input = $('.users-search input').prop("value");
+    this.$ul = $('.users-search ul');
     // this.input = $el.data();
+  }
+
+  searchUsers(queryVal, success) {
+    $.ajax({
+      url: "users/search",
+      dataType: "json",
+      method: "GET",
+      data: {queryVal}
+    });
+  }
+
+  handleInput () {
+    // input? do things?
+    this.searchUsers(/*more things*/);
   }
 
 }
