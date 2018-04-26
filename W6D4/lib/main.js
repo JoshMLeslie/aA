86528a448540dc3208a28ac2c50b6765a664d536
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-04-26 17:54:05 -0400

    late W6D4 update

--- W6D4/lib/main.js
+++ W6D4/lib/main.js
@@ -15,11 +15,63 @@ function $l(selector) {
   }
   return new DOMNodeCollection(result);
 
+
+
+
 }
 
 window.$l = $l;
 
-$l( () => {
-  setTimeout( () => {
-       alert('page loaded'); } , 1500); }
-);
+// $l( () => {
+//   setTimeout( () => {
+//        alert('page loaded'); } , 1500); }
+// );
+
+
+
+$l.extend = function(...args) {
+  let first = Object.assign({}, args[0]);
+  for (let i = 1; i < args.length; i++){
+    first = Object.assign(first, args[i]);
+  }
+  return first;
+};
+
+$l.ajax = function (options) {
+  let defaults = {
+    success: console.log("success!"),
+    error: console.log("error!"),
+    url: "http://www.google.com",
+    method: "GET",
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8'
+  };
+
+  options = $l.extend(defaults, options);
+
+  const xhr = new XMLHttpRequest();
+  xhr.open(options.method, options.url);
+  xhr.onload = function () {
+    console.log(xhr.status); // for status info
+    console.log(xhr.responseType); //the type of data that was returned
+    console.log(xhr.response); //the actual response. For JSON api calls, this will be a JSON string
+  };
+
+  // const optionalData = { name: "User1", password : "123456" };
+  xhr.send(optionalData);
+
+
+
+
+};
+
+$.ajax({
+      type: 'GET',
+      url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=bcb83c4b54aee8418983c2aff3073b3b",
+      success(data) {
+        console.log("We have your weather!");
+        console.log(data);
+      },
+      error() {
+        console.error("An error occurred.");
+      },
+   });
