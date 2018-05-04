96b45c20352d1a5b304b291538f0ca8d3f8f79d0
joshmleslie <hello@joshmleslie.com>
2018-05-04 14:37:39 -0400

    init benchbnb

---
+++ WRK/W7/D4/BenchBnB/app/controllers/application_controller.rb
@@ -0,0 +1,29 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def login(user)
+    @current_user = user
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+  end
+
+  def logout
+    current_user.reset_session_token
+    session[:session_token] = nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session: session[:session_token])
+  end
+
+  def require_login
+    render json: { base: ['invalid credentials'] }, status: 401 unless current_user
+  end
+end
